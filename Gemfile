b92e3eb28586f320c13054efb9e6b095e749ea2c
Gergo Angyal <angyalg@gmail.com>
2017-08-17 17:10:30 +0200

    all rake green ;

--- Gemfile
+++ Gemfile
@@ -7,6 +7,11 @@ end
 
 ruby '2.3.4'
 
+gem 'jquery-rails'
+gem "bootstrap-sass"
+gem "font-awesome-sass"
+gem "simple_form"
+gem "autoprefixer-rails"
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.3'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "white rum")
+Ingredient.create(name: "whiskey")
+
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Death in the Afternoon")
+Cocktail.create(name: "Green Russians")
+
+Dose.create(description: "3", cocktail_id: 1, ingredient_id: 1)
+Dose.create(description: "2", cocktail_id: 1, ingredient_id: 2)
+Dose.create(description: "2", cocktail_id: 1, ingredient_id: 3)
+Dose.create(description: "4 cl", cocktail_id: 1, ingredient_id: 4)
+
+Dose.create(description: "3", cocktail_id: 2, ingredient_id: 1)
+Dose.create(description: "2", cocktail_id: 2, ingredient_id: 2)
+Dose.create(description: "4 cl", cocktail_id: 2, ingredient_id: 5)
+
+Dose.create(description: "3", cocktail_id: 3, ingredient_id: 1)
+Dose.create(description: "2", cocktail_id: 3, ingredient_id: 2)
+Dose.create(description: "4 cl", cocktail_id: 3, ingredient_id: 5)
+
